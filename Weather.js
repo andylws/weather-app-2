82c79d27b9fd4f3071ce4c20c35cca336f5bb17c
andylws <andylws@naver.com>
2021-05-14 20:47:12 +0900

    added title and subtitle for each weather

--- Weather.js
+++ Weather.js
@@ -8,22 +8,32 @@ const weatherOptions = {
   Thunderstorm: {
     iconName: "weather-lightning",
     gradient: ["#485563", "#29323c"],
+    title: "Thunderstorm",
+    subtitle: "Electric Shock!",
   },
   Drizzle: {
     iconName: "weather-hail",
     gradient: ["#D7DDE8", "#757F9A"],
+    title: "Drizzle",
+    subtitle: "Nice to get soaked in the rain :)",
   },
   Rain: {
     iconName: "weather-pouring",
     gradient: ["#859398", "#283048"],
+    title: "Rainy",
+    subtitle: "Don't forget your umbrella.",
   },
   Snow: {
     iconName: "weather-snowy",
     gradient: ["#E2E2E2", "#C9D6FF"],
+    title: "Snowy",
+    subtitle: "Winter wonderland",
   },
   Fog: {
     iconName: "weather-fog",
     gradient: ["#859398", "#283048"],
+    title: "Foggy",
+    subtitle: "Watch out!",
   },
   Clear: {
     iconName: "weather-sunny",
@@ -34,38 +44,56 @@ const weatherOptions = {
   Clouds: {
     iconName: "weather-cloudy",
     gradient: ["#bdc3c7", "#2c3e50"],
+    title: "Cloudy",
+    subtitle: "Gloomy Blue.",
   },
   Mist: {
     iconName: "weather-hazy",
     gradient: ["#E0EAFC", "#CFDEF3"],
+    title: "Misty",
+    subtitle: "Everybody moist Skin! LOL",
   },
   Smoke: {
     iconName: "weather-hazy",
     gradient: ["#eef2f3", "#8e9eab"],
+    title: "Smoky",
+    subtitle: "No smoking.",
   },
   Haze: {
     iconName: "weather-hazy",
     gradient: ["#D7DDE8", "#757F9A"],
+    title: "Hazy",
+    subtitle: "Can you see me?",
   },
   Dust: {
     iconName: "weather-hazy",
     gradient: ["#EAEAEA", "#DBDBDB", "#F2F2F2", "#ADA996"],
+    title: "Dust",
+    subtitle: "MASK!",
   },
   Sand: {
     iconName: "weather-hazy",
     gradient: ["#DECBA4", "#3E5151"],
+    title: "Sand",
+    subtitle: "MASK! MASK!",
   },
   Ash: {
     iconName: "weather-fog",
     gradient: ["#606c88", "#3f4c6b"],
+    title: "Sunny",
+    subtitle: "MASK! MASK! MASK!",
   },
   Squall: {
     iconName: "weather-windy",
     gradient: ["#BBD2C5", "#536976"],
+    title: "Squall",
+    subtitle: "Hold on your hat!",
   },
   Tornado: {
     iconName: "weather-tornado",
     gradient: ["#414345", "#232526"],
+    title: "Tornado",
+    subtitle: "Keep in home safely.",
   },
 };
 
